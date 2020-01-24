bc0cb85d2d6d205f0d8c9016e53fdc757258f634
ml <m.lesnierowski@gmail.com>
2020-01-24 21:45:09 +0100

    Load features and labels

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import argparse
-from utils import TrainDataLoader
+from utils import BatchLoader
 
 parser = argparse.ArgumentParser(description='Predict the emotion related to the tweet.')
 parser.add_argument('-d', '--train-data', help='path to training .csv data', type=str)
 args = parser.parse_args()
 
-train_data = TrainDataLoader(args.train_data)
-
-with train_data as data:
-    print(data.head())
+batch_loader = BatchLoader(args.train_data)
+with batch_loader as batch:
+    print(batch.features)
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,34 @@
 import numpy as np
 import pandas as pd
+from collections import namedtuple
 
-class TrainDataLoader():
+class Batch():
+     def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+
+
+class BatchLoader():
     def __init__(self, path):
         self.path = path
-        self.data = None
+        self.batch = None
 
     def __enter__(self):
+        data = None
         try:
-            self.data = pd.read_csv(self.path)
-
+            data = pd.read_csv(self.path)
             print("Train data loaded.")
-
-            return self.data
         except Exception as e:
             print("Train data could not be loaded: {}".format(e))
+
+        if data is not None:
+            features = data.iloc[:, 2].values
+            labels = data.iloc[:, 1].values
+
+        batch = Batch(features, labels)
+
+        self.batch = batch
+        return self.batch
         
     def __exit__(self, type, value, traceback):
         pass
