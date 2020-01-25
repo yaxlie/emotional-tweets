cc646255bf901400bd0c1aa91bcccba234c4f265
ml <m.lesnierowski@gmail.com>
2020-01-25 01:38:57 +0100

    Improve testing

--- src/main.py
+++ src/main.py
@@ -1,19 +1,15 @@
 import argparse
 from train import train
-from test import test
 from predict import predict_to_csv
 
 parser = argparse.ArgumentParser(description='Predict the emotion related to the tweet.')
 parser.add_argument('-d', '--train-data', help='path to training .csv data', type=str)
-parser.add_argument('-t', '--test-data', help='path to testing .csv data', type=str)
+parser.add_argument('-t', '--test', help='do you want to split the data to test sets', type=bool, default=False)
 parser.add_argument('-p', '--predict-data', help='path to .csv file to predict', type=str)
 parser.add_argument('-o', '--output', help='output file', type=str)
 args = parser.parse_args()
 
-model, vectorizer = train(args.train_data)
-
-if args.test_data:
-    test(model, vectorizer, args.test_data)
+model, vectorizer = train(args.train_data, args.test)
 
 if args.predict_data:
     dest = args.output if args.output else 'predictions.csv'
--- src/train.py
+++ src/train.py
@@ -3,15 +3,29 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-def train(data):
+
+def train(data, test=False):
     batch_loader = BatchLoader(data)
     with batch_loader as batch:
         print('Preparing data...')
 
+        if test:
+            X_train, X_test, y_train, y_test = train_test_split(batch.features, batch.labels, test_size=0.2, random_state=0)
+        else:
+            X_train = batch.features
+            y_train = batch.labels
+
         print('Train...')
-        text_classifier = RandomForestClassifier(n_estimators=200, random_state=0)
-        text_classifier.fit(batch.features, batch.labels)
+        text_classifier = RandomForestClassifier(n_estimators=10, random_state=0)
+        text_classifier.fit(X_train, y_train)
 
         print('Training finished!')
+
+        if test:
+            predictions = text_classifier.predict(X_test)
+
+            print(confusion_matrix(y_test, predictions))
+            print(classification_report(y_test, predictions))
+            print(accuracy_score(y_test, predictions))
 
         return text_classifier, batch_loader.vectorizer
